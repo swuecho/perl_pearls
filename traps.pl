2b3201b0ba21d272cb608b5173715950e2124192
Hao Wu <echowuhao@gmail.com>
2014-06-13 15:14:28 -0700

    Create traps.pl

---
+++ traps.pl
@@ -0,0 +1,5 @@
+echo@Think:~$ perl -E 'say((split "a", "ababc")[1])'
+b
+echo@Think:~$ perl -E 'say (split "a", "ababc")[1]'
+syntax error at -e line 1, near ")["
+Execution of -e aborted due to compilation errors.
